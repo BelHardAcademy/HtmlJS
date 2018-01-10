0530a487966f44fa71f07394fe1c710b98f286b4
Sergey Vishnevsky <svishnevskij@gmail.com>
2018-01-10 16:36:53 +0300

    Move blog page to angular

--- src/js/app/directives/modal.directive.js
+++ src/js/app/directives/modal.directive.js
@@ -1,16 +1,22 @@
-(function(){
+(function () {
     'use strict'
 
     angular
         .module('app')
         .directive('modal', modal);
 
-    function modal(){
+    function modal() {
         return {
             restrict: 'A',
-            link: function(scope, element, attrs){
-                element.on('click', function(){
-                    $(element).modal();
+            link: function (scope, element, attrs) {
+                element.on('click', function () {
+                    var target = element;
+                    if (attrs.target) {
+                        target = '#' + attrs.target;
+                    }
+
+                    $(target).modal();
+                    return false;
                 });
             }
         }
