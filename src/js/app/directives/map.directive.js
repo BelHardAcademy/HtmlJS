935c2c4da71ceb56cecd76043ad70bde2586461e
Sergey Vishnevsky <s.vishnevsky@itransition.com>
2018-01-24 13:52:13 +0300

    Add map directive

--- src/js/app/directives/map.directive.js
+++ src/js/app/directives/map.directive.js
@@ -5,7 +5,6 @@
         .module('app')
         .directive('map', map);
 
-    var mapNumber = 0;
     var defaultSettings = {
         center: [53.918474, 27.583611],
         zoom: 12
@@ -14,27 +13,62 @@
     function map() {
         return {
             restrict: 'E',
-            template: '<div id="{{mapId}}" class="map" style="width:100%; height: 500px"></div>',
             scope: {
                 settings: '=',
                 onGetPosition: '&'
             },
-            link: function (scope, element, attrs) {
-                scope.mapId = `map-${mapNumber++}`;
+            transclude: true,
+            link: function ($scope, element, attrs, ctrl, transcludeFn) {
+                transcludeFn(function (copy) {
+                    element.append(copy);
+                });
+            },
+            controller: ['$scope', '$element', function ($scope, $element) {
+                var self = this;
                 ymaps.load().then(function (ymaps) {
                     ymaps.ready(function () {
-                        var map = new ymaps.Map(scope.mapId, angular.extend({}, defaultSettings, scope.settings));
-
-                        if (scope.onGetPosition && navigator.geolocation) {
+                        var map = new ymaps.Map($element[0], angular.extend({}, defaultSettings, $scope.settings));
+                        $scope.$broadcast('ready');
+                        if ($scope.onGetPosition && navigator.geolocation) {
                             navigator.geolocation.getCurrentPosition(function (position) {
-                                scope.onGetPosition(position.coords);
+                                $scope.onGetPosition({
+                                    coords: position.coords
+                                });
                             }, function (error) {
                                 console.log(error.message);
                             });
                         }
+
+                        self.addRoute = function (points) {
+                            var multiRoute = new ymaps.multiRouter.MultiRoute({
+                                referencePoints: points,
+                                params: {
+                                    routingMode: 'pedestrian'
+                                }
+                            }, {
+                                boundsAutoApply: true
+                            });
+                            map.geoObjects.add(multiRoute);
+                            return multiRoute;
+                        }
+
+                        self.removeRoute = function (route) {
+                            map.geoObjects.remove(route);
+                        }
+
+                        self.addMarker = function (coordinates) {
+                            var placeMark = new ymaps.Placemark(coordinates);
+                            $scope.markers.add(placeMark);
+
+                            return placeMark;
+                        };
+
+                        self.removeMarker = function (marker) {
+                            $scope.geoObjects.remove(marker);
+                        };
                     });
                 });
-            }
+            }]
         }
     }
 })();
